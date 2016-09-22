29a0196e433e6ff85a995d2cb103a3119b65283d
eleanorstrib <eleanor@sloan.mit.edu>
2016-09-21 19:29:28 -0700

    added static folder, confirmed that info is going to database but messages not working

--- productized/signup/forms.py
+++ productized/signup/forms.py
@@ -1,5 +1,8 @@
+from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
-class signUpForm(forms.ModelForm):
-	model = User
-	fields = ('first_name', 'last_name', 'email')
+class Prelaunch_signup(forms.ModelForm):
+	class Meta: 
+		model = User
+		fields = ('first_name', 'last_name', 'email')
--- productized/signup/views.py
+++ productized/signup/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from .forms import Prelaunch_signup
 
-# Create your views here.
+def plsignup(request):
+	form = Prelaunch_signup()
+	if request.method == 'POST':
+		form = Prelaunch_signup(request.POST)
+		if form.is_valid():
+			form.save()
+
+	return render(request, 'signup/plsignup.html', {'form': form})
+
+
